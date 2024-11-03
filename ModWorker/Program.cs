7370f2b9d5115e5838caca079fdb4a1f4ed7e0ca
Zuxi <gpg@mail.imzuxi.com>
2024-11-03 02:00:18 -0500

    init Files

---
+++ ModWorker/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.IO.Compression;
+using System.Threading;
+
+namespace ModWorker
+{
+    internal class Program
+    {
+        private static string _workingPath = null;
+        public static void Main(string[] args)
+        {
+
+            _workingPath = Directory.GetCurrentDirectory();
+            string modsPath = Path.Combine(_workingPath, "Mods");
+            // Because Windows Lol
+            if (_workingPath != null)
+            {
+                try {
+                    Process.GetProcessesByName("VRChat.exe").FirstOrDefault()?.Kill();
+                }
+                catch (Exception)
+                {
+                    // safely ignore this exception
+                }
+
+                string[] compressedMods = GetAllZipFilesInFolder(modsPath);
+
+                foreach (var compressedMod in compressedMods)
+                {
+                    Console.WriteLine("Extracting {0} to {1}", compressedMod, _workingPath);
+                    ExtractZipFile(compressedMod, Directory.GetCurrentDirectory());
+                    File.Delete(compressedMod);
+                }
+            }
+            Thread.Sleep(1000);
+            
+            File.Delete(Path.Combine(Directory.GetCurrentDirectory(),"Plugins", "ModInstaller.dll"));
+          
+           StartVRChatAgain();
+        }
+
+        private static string[] GetAllZipFilesInFolder(string path)
+        {
+            var filePaths = Directory.GetFiles(path, "*.zip", SearchOption.AllDirectories);
+
+            FileInfo[] files = filePaths.Select(filePath => new FileInfo(filePath)).ToArray();
+
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine($"Found zip file: {file.FullName}");
+            }
+            return filePaths;
+        }
+
+        private static void ExtractZipFile(string zipPath, string extractPath)
+        {
+            try
+            {
+                using (ZipArchive archive = ZipFile.OpenRead(zipPath))
+                {
+                    foreach (ZipArchiveEntry entry in archive.Entries)
+                    {
+                        // Get the full destination path
+                        string destinationPath = Path.Combine(extractPath, entry.FullName);
+
+                        string directoryPath = Path.GetDirectoryName(destinationPath);
+                        if (!Directory.Exists(directoryPath))
+                        {
+                            Directory.CreateDirectory(directoryPath);
+                        }
+
+                        if (!string.IsNullOrEmpty(entry.Name))
+                        {
+                            if (File.Exists(destinationPath))
+                            {
+                                File.Delete(destinationPath);
+                            }
+                            entry.ExtractToFile(destinationPath, overwrite: true);
+                        }
+                    }
+                }
+
+                Console.WriteLine("WroteFiles");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private static void StartVRChatAgain()
+        {
+            string arguments = "--modWorkerRan";
+            foreach (string stringi in Environment.GetCommandLineArgs())
+            {
+                arguments += $"{stringi} ";
+            }
+            System.Diagnostics.Process vrcProcess = new System.Diagnostics.Process();
+            vrcProcess.StartInfo.FileName = Directory.GetCurrentDirectory() + "\\VRChat.exe";
+            vrcProcess.StartInfo.Arguments = arguments;
+            vrcProcess.Start();
+        }
+        
+    }
+}
---
+++ ModWrapper/MainEntry.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using MelonLoader;
+using MelonLoader.ICSharpCode.SharpZipLib.Zip;
+[assembly: MelonInfo(typeof(ModWrapper.MainEntry), "ModInstaller", "1.0.0", "Zuxi", "")]
+namespace ModWrapper
+{
+    public class MainEntry : MelonPlugin
+    {
+        MainEntry()
+        {
+            UnpackAndRunInstaller();
+            StartInstaller();
+            Process.GetCurrentProcess().Kill();
+        }
+        
+        private static void UnpackAndRunInstaller()
+        {
+            string baseModWorkerPath = Path.Combine(MelonUtils.BaseDirectory, "ModWorker.exe");
+            byte[] rawData = ExtractResource("ModWrapper.ModWorker.exe");
+            if (File.Exists(baseModWorkerPath))
+            {
+                File.Delete(baseModWorkerPath);
+            }
+            File.WriteAllBytes(baseModWorkerPath, rawData); 
+            
+        }
+        
+        private static void StartInstaller()
+        {
+            string arguments = "--modWorkerPath=" + MelonUtils.BaseDirectory;
+            foreach (string stringi in Environment.GetCommandLineArgs())
+            {
+                arguments += $"{stringi} ";
+            }
+            System.Diagnostics.Process vrcProcess = new System.Diagnostics.Process();
+            vrcProcess.StartInfo.FileName = Directory.GetCurrentDirectory() + "\\ModWorker.exe";
+            vrcProcess.StartInfo.Arguments = arguments;
+            vrcProcess.Start();
+        }
+
+        private static byte[] ExtractResource(string filename)
+        {
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+            Stream stream = executingAssembly.GetManifestResourceStream(filename);
+            if (stream == null)
+            {
+                return null;
+            }
+            byte[] array = new byte[stream.Length];
+            var _ = stream.Read(array, 0, array.Length);
+            return array;
+        }
+    }
+}
